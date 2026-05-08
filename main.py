c183eab0781e450a673f875676df4bc1623cc614
shshahid <shahids1@sunypoly.edu>
2026-05-07 20:12:34 -0400

    Generalize dataset input

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score
 
 #load dataset - should be in same directory as main.py
 data = pd.read_csv("spam_ham_dataset.csv")
+#error check
 if data.empty:
    print("ERROR: dataset not loaded.\n")
    exit(1)
@@ -15,26 +16,32 @@ else:
     print("SUCCESS: Dataset loaded.\n")
     print(data.head())
 
-X = data['text']
-Y = data['label_num']
-#80% train, 20% test
+#dataset should have at least "text" and "label" - convert label to 1/0s
+data.loc[data["label"].str.casefold() == "spam", "res"] = 1
+data.loc[data["label"].str.casefold() == "ham", "res"] = 0
+print(data.head())
+
+#assign X, Y
+X = data['text'].astype("U")
+Y = data['res']
+#split data, 80% train, 20% test
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 
-#TFIDF convert text into numerical feature - 
-#parameters: retains all tokens, removes standard stop words, and converts all lowercase
+#tokenization -- TFIDF converts text into numerical features
+#parameters: retains all tokens, removes standard stop words from English list, and converts all lowercase
 feature_extraction = TfidfVectorizer(min_df = 1, stop_words = 'english', lowercase = True)
 X_train_features = feature_extraction.fit_transform(X_train)
 X_test_features = feature_extraction.transform(X_test)
 
-Y_train = Y_train.astype(int)
-Y_test = Y_test.astype(int)
-
+#choose logistic regression model
 model = LogisticRegression()
+#model training
 model.fit(X_train_features, Y_train)
 
+#evaluation on train
 train_prediction = model.predict(X_train_features)
 train_accuracy = accuracy_score(Y_train, train_prediction)
-
+#evaluation on test
 test_prediction = model.predict(X_test_features)
 test_accuracy = accuracy_score(Y_test, test_prediction)
 
